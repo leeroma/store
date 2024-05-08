73acecc66e1545a565f7b60c6f09625aa1dab334
Roma <roma.li0071@gmail.com>
2024-05-08 18:47:25 +0500

    added qty field in product

--- storeapp/models.py
+++ storeapp/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
 
@@ -18,6 +19,8 @@ class Product(models.Model):
     description = models.TextField('Описание', max_length=3000, blank=True)
     category = models.ManyToManyField(Category, verbose_name='Категория', related_name='category')
     created_at = models.DateTimeField('Дата создания', auto_now_add=True)
+    quantity = models.DecimalField('Количество', max_digits=5, decimal_places=0, default=1,
+                                   validators=[MinValueValidator(1)])
     price = models.DecimalField('Цена', max_digits=10, decimal_places=2)
     image = models.ImageField('Изображение', upload_to='products')
 
